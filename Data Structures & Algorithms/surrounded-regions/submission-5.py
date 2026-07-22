bfd20115c358f6ec977f9266f8c3b4ad462d1894
Kevin Rusagara <144623341+rusagara-ops@users.noreply.github.com>
2026-07-22 13:34:52 -0400

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return 
+
+        cols = len(board[0])
+        rows = len(board)
+
+        def capture(r,c):
+    
+            if r < 0 or c < 0 or r == rows or c == cols or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            capture(r+1,c)
+            capture(r-1,c)
+            capture(r,c+1)
+            capture(r, c-1)
+
+        # capture all the cells on the edge and find the unsorrounded regions O to T
+        for r in range(rows):
+            for c in range(cols):
+                if (board[r][c] == "O" and (r in [0,rows-1] or c in [0, cols-1])):
+                    capture(r,c)
+        # changes the middle surrounded regions O to X
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
+                
+
+        # uncapture the unsurrounded regions
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if board[r][c] == "T":
+        #             board[r][c] = "O"
