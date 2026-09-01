d11a4827347c459a1c2ff135a1dbaa505f6eef2d
Kevin Rusagara <144623341+rusagara-ops@users.noreply.github.com>
2026-08-31 20:12:30 -0400

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endofword = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+
+        curr.endofword = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+            for i in range(j,len(word)):
+                char = word[i]
+                if char == ".": #we use backttracking/ recursion to go down the 26 characters
+                    for child in curr.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if char not in curr.children:
+                        return False
+                    curr = curr.children[char]
+            return curr.endofword
+
+        return dfs(0,self.root)
+        
